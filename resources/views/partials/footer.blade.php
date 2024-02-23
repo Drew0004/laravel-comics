af763ab061c84f3e0481cbe8d5b87b00ea483aa5
Drew0004 <drdrewgraphic@gmail.com>
2024-02-23 19:07:15 +0100

    Completato esercizio di base

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -31,9 +31,75 @@
         'SHOP'=>[
             'Shop DC',
             'Shop DC collectibles'
-        ]
+        ],
+];
+    $socialFooter =[
+        'footer-facebook.png',
+        'footer-twitter.png',
+        'footer-youtube.png',
+        'footer-pinterest.png',
+        'footer-periscope.png'
     ]
 @endphp
 <footer>
-    Footer
+    <div class="upper-footer py-5">
+        
+        <div class="container">
+            <div class="row  justify-content-between">
+                <div class="col-6 d-flex flex-wrap align-items-start">
+                    @foreach($linksFooter as $arrayName => $singleElem)
+                    <div class="col-4">
+                        <ul class="list-unstyled">
+                            <h4 class="text-white">{{$arrayName}}</h4>
+                            @foreach ($singleElem as $singleList)
+                                <li>
+                                    <a class="text-decoration-none" href="#nogo">{{$singleList}}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endforeach
+                    <p>
+                        All site contents TM and &copy; 2020 DC entertainment, unless otherwhise
+                        <a class="foot-link" href="#nogo">noted here</a>. All rights reserved.
+                        <a class="foot-link" href="#nogo">Cookies Settings</a>
+                    </p>
+                </div>
+
+                <div class="col-6">
+                    <div class="overflow-logo">
+                        <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="Logo Background">
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+    
+    </div>
+
+    <div class="lower-footer py-4">
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-2">
+                    <button class="foot-button">
+                        SIGN UP NOW!
+                    </button>
+                </div>
+    
+                <div class="col-8 d-flex justify-content-end align-items-center">
+                    <h3 class="me-4">Follow us</h3>
+        
+                    <ul class="list-unstyled d-flex mb-0">
+                        @foreach ($socialFooter as $singleSocial)
+                            <li class="px-2">
+                                <a href="#nogo">
+                                    <img src="{{Vite::asset('resources/img/'.$singleSocial)}}" alt="Social">
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
